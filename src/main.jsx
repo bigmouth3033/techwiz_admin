e128ec75bff00cbf63aa0da5b43051d2859ef14f
tan <bigmouth3033@gmail.com>
2024-10-03 15:43:29 +0700

    before deploy

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <RouterProvider router={router} />
+      <RouterProvider router={router} basename="/techwiz_admin" />
     </QueryClientProvider>
   </StrictMode>
 );
